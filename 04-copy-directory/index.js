fe1515f4a9f80211aedaad018c5ac6cde1b958f7
evgueniazet <zelenkoea19@gmail.com>
2022-11-08 15:55:10 +0200

    fix: changed the solution of 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,20 +1,60 @@
 const fs = require('fs');
+const filesCopy = '04-copy-directory/files-copy';
+const files = '04-copy-directory/files';
+
+// const readDirAsync = (dirname) => {
+//   return new Promise((resolve, reject) => {
+//     fs.readdir(dirname, { withFileTypes: true }, (err) => {
+//       if (err) reject(err); else resolve(buffer);
+//     });
+//   });
+// };
+
 const copyDir = () => {
 
-  fs.mkdir('04-copy-directory/files-copy', { recursive: true }, err => {
-    if (err) throw console.log('Папка уже создана!');
-  });
-
-  fs.readdir('04-copy-directory/files', (err, files) => {
-    if (err) throw err;
-
-    files.forEach(file => {
-      fs.copyFile(`04-copy-directory/files/${file}`, `04-copy-directory/files-copy/${file}`, err => {
-         if(err) throw err; 
+  fs.stat(filesCopy, function (err) {
+    if (!err) {
+      // console.log('Папка существует');
+      fs.readdir(filesCopy, (err, filesCopy) => {
+        if (err) throw err;
+        filesCopy.forEach(fileCopy => {
+          fs.unlink(`04-copy-directory/files-copy/${fileCopy}`, function(err){
+            if (err) {
+                console.log(err);
+            } else {
+                // console.log("Файл удалён");
+                fs.readdir(files, (err, files) => {
+                  if (err) throw err;
+          
+                  files.forEach(file => {
+                    fs.copyFile(`04-copy-directory/files/${file}`, `04-copy-directory/files-copy/${file}`, err => {
+                      if (err) throw err;
+                    });
+                  })
+                });
+            }
+        });
+        })
       });
-    })
-  });
+    }
+    else if (err.code === 'ENOENT') {
+      fs.mkdir(filesCopy, { recursive: true }, err => {
+        if (err) throw console.log('Папка уже создана!');
+      });
 
+      fs.readdir(files, (err, files) => {
+        if (err) throw err;
+
+        files.forEach(file => {
+          fs.copyFile(`04-copy-directory/files/${file}`, `04-copy-directory/files-copy/${file}`, err => {
+            if (err) throw err;
+          });
+        })
+      });
+    }
+  });
 };
 
 copyDir();
+
+
