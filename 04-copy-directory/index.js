06e7142453a1ab95a416591b444a46ba2f2eeaa9
evgueniazet <zelenkoea19@gmail.com>
2022-11-08 16:37:38 +0200

    fix: changed the solution of 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,33 +2,21 @@ const fs = require('fs');
 const filesCopy = '04-copy-directory/files-copy';
 const files = '04-copy-directory/files';
 
-// const readDirAsync = (dirname) => {
-//   return new Promise((resolve, reject) => {
-//     fs.readdir(dirname, { withFileTypes: true }, (err) => {
-//       if (err) reject(err); else resolve(buffer);
-//     });
-//   });
-// };
-
 const copyDir = () => {
 
   fs.stat(filesCopy, function (err) {
-    if (!err) {
-      // console.log('Папка существует');
+    if (!err) {     
       fs.readdir(filesCopy, (err, filesCopy) => {
         if (err) throw err;
         filesCopy.forEach(fileCopy => {
           fs.unlink(`04-copy-directory/files-copy/${fileCopy}`, function(err){
             if (err) {
                 console.log(err);
-            } else {
-                // console.log("Файл удалён");
+            } else {                
                 fs.readdir(files, (err, files) => {
-                  if (err) throw err;
-          
+                  if (err) throw err;          
                   files.forEach(file => {
-                    fs.copyFile(`04-copy-directory/files/${file}`, `04-copy-directory/files-copy/${file}`, err => {
-                      if (err) throw err;
+                    fs.copyFile(`04-copy-directory/files/${file}`, `04-copy-directory/files-copy/${file}`, err => {                      
                     });
                   })
                 });
